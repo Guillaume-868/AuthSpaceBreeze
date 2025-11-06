d9e08344516c9d6ec6dd380497befd130e6e04c8
grobidet <guillaume.robidet@outlook.fr>
2025-11-06 17:04:30 +0100

    View index Modify

--- resources/views/Space/PlanetsCrud/index.blade.php
+++ resources/views/Space/PlanetsCrud/index.blade.php
@@ -9,48 +9,6 @@
         + Ajouter une planète
     </a>
 
-    <!-- Table en français -->
-    <div class="overflow-x-auto mt-10 px-10">
-        <h2 class="text-2xl font-semibold mb-4 text-center">🇫🇷 Planètes (Français)</h2>
-
-        <table class="min-w-full bg-white border border-gray-200 shadow-md rounded text-center">
-            <thead class="bg-gray-100">
-                <tr>
-                    <th class="py-2 px-4 border-b">Nom</th>
-                    <th class="py-2 px-4 border-b">Sous-titre</th>
-                    <th class="py-2 px-4 border-b">Distance</th>
-                    <th class="py-2 px-4 border-b">Durée</th>
-                    <th class="py-2 px-4 border-b">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($planets as $planet)
-                    <tr class="hover:bg-gray-50">
-                        <td class="py-2 px-4 border-b">{{ $planet->name }}</td>
-                        <td class="py-2 px-4 border-b">{{ $planet->subtitle}}</td>
-                        <td class="py-2 px-4 border-b">{{ $planet->distance}}</td>
-                        <td class="py-2 px-4 border-b">{{ $planet->duration}}</td>
-                        <td class="py-2 px-4 border-b space-x-2">
-                            <a href="{{ route('planets.edit', $planet) }}"
-                               class="bg-yellow-400 hover:bg-yellow-500 text-white py-1 px-4 rounded inline-block w-24 text-center">
-                                Modifier
-                            </a>
-
-                            <form action="{{ route('planets.destroy', $planet) }}" method="POST" class="inline-block">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit"
-                                        class="bg-red-500 hover:bg-red-600 text-white py-1 px-4 rounded inline-block w-24 text-center">
-                                    Supprimer
-                                </button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-
     <!-- Table en anglais -->
     <div class="overflow-x-auto mt-20 px-10 mb-10">
         <h2 class="text-2xl font-semibold mb-4 text-center">🇬🇧 Planets (English)</h2>
